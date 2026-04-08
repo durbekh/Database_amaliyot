9898e918bfcaf655817fd9e15429170e5a0e8524
Durbek Hayotbekov <d83217311@gmail.com>
2026-04-08 15:49:45 +0500

    yangilandi

--- db1.py
+++ db1.py
@@ -9,6 +9,8 @@ cur.execute("""CREATE TABLE IF NOT EXISTS users(
     yosh INTEGER
 )""")
 cur.execute("""CREATE TABLE IF NOT EXISTS xaridlar(
+    id INTEGER,
+    user_id INTEGER,
     nomi TEXT,
     narxi INTEGER
 )""")
@@ -16,6 +18,10 @@ cur.execute("""CREATE TABLE IF NOT EXISTS xaridlar(
 cur.execute("""INSERT INTO users(id , ism , fam , yosh) VALUES (1, 'Durbek' , 'Hayotbekov', 18)""")
 cur.execute("""INSERT INTO users(id , ism , fam , yosh) VALUES (2, 'Farhotjon' , 'Abdullayev', 25)""")
 cur.execute("""INSERT INTO users(id , ism , fam , yosh) VALUES (3, 'Anvar' , 'Jovliyev', 19)""")
+
+cur.execute("""INSERT INTO xaridlar(id , user_id, nomi , narxi) VALUES (1, 1 ,'Olma', 25_000)""")
+cur.execute("""INSERT INTO xaridlar(id , user_id, nomi , narxi) VALUES (2, 2 ,'Uzum', 30_000)""")
+
 
 con.commit()
 con.close()
